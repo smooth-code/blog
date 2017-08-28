b5c336d7f1ce3f0b33b5049a6a0e304961a7d363
Greg Bergé <berge.greg@gmail.com>
2017-08-28 14:35:03 +0200

    Add cloudinary storage

---
+++ content/adapters/storage/cloudinary.js
@@ -0,0 +1,46 @@
+const cloudinary = require('cloudinary');
+const BaseAdapter = require('ghost-storage-base');
+
+class CloudinaryStore {
+  constructor(config = {}) {
+    super();
+    this.config = config;
+    cloudinary.config(config);
+  }
+
+  save(image) {
+    const secure = this.config.secure || false;
+
+    return new Promise(function(resolve) {
+      cloudinary.uploader.upload(image.path, function(result) {
+        resolve(secure ? result.secure_url : result.url);
+      });
+    });
+  }
+
+  delete(image) {
+    return new Promise(function(resolve) {
+      cloudinary.uploader.destroy('zombie', function(result) {
+        resolve(result)
+      });
+    });
+  }
+
+  exists(filename) {
+    return new Promise(function(resolve) {
+      if (cloudinary.image(filename, { })) {
+          resolve(true);
+      } else {
+          resolve(false);
+      }
+    });
+  }
+
+  serve() {
+    return function (req, res, next) {
+      next();
+    };
+  }
+}
+
+module.exports = CloudinaryStore;
