5294de0fc29645372515fbd388936938800cbd70
Greg Bergé <berge.greg@gmail.com>
2017-08-28 12:50:34 +0200

    Log theme

--- core/server/themes/validate.js
+++ core/server/themes/validate.js
@@ -9,6 +9,8 @@ checkTheme = function checkTheme(theme, isZip) {
         // gscan can slow down boot time if we require on boot, for now nest the require.
         gscan = require('gscan');
 
+    console.log('THEME', theme.path)
+
     if (isZip) {
         checkPromise = gscan.checkZip(theme, {
             keepExtractedDir: true
@@ -17,7 +19,6 @@ checkTheme = function checkTheme(theme, isZip) {
         checkPromise = gscan.check(theme.path);
     }
 
-    console.log('THEME', theme.path)
 
     return checkPromise.then(function resultHandler(checkedTheme) {
         checkedTheme = gscan.format(checkedTheme, {
